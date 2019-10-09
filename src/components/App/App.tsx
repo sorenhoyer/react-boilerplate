de9b131d74c346c8359b4bb0baa267e62370a316
Søren Høyer <sorenhoyer84@gmail.com>
2019-10-10 00:05:59 +0200

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App: React.FC<any> = () => (
+  <div>App</div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const APP_PATH = path.resolve(__dirname, 'src');
+
+module.exports = {
+  entry: APP_PATH,
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  module: {
+    rules: [{
+      test: /\.(ts|js)x?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(APP_PATH, 'index.html'),
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ]
+};
